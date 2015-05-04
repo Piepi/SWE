fe7bd222487662238c1ec6947fbf0d39791d84d9
Kingmakke <markus.grothues@freenet.de>
2015-05-04 14:48:34 +0200

    Initial commit

---
+++ app/src/main/java/com/hs_osnabrueck/swe_app/myapplication/AchievementFragment.java
@@ -0,0 +1,21 @@
+package com.hs_osnabrueck.swe_app.myapplication;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AchievementFragment extends Fragment {
+
+    public AchievementFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_achievement, container, false);
+    }
+}
