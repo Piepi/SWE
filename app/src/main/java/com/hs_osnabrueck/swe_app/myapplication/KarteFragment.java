aaf36dbac7954b0f8fb0c5c9d2c07e349e99d329
Kingmakke <markus.grothues@freenet.de>
2015-05-07 11:51:12 +0200

    TEST HS

--- app/src/main/java/com/hs_osnabrueck/swe_app/myapplication/KarteFragment.java
+++ app/src/main/java/com/hs_osnabrueck/swe_app/myapplication/KarteFragment.java
@@ -20,6 +20,7 @@ public class KarteFragment extends Fragment {
         // Inflate the layout for this fragment
         rootView = inflater.inflate(R.layout.fragment_karte, container, false);
         //MAP
+        // test
         return rootView;
     }
 }
